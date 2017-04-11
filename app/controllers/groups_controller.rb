ab90104d1608195f1de1fae741fc22e438649629
zhoujiqing <jiqing.zhou@gmail.com>
2017-04-11 15:50:15 +0800

    add validation to title apply  to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,9 +18,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-      redirect_to groups_path
+      if @group.save
+        redirect_to groups_path
+      else
+        render  :new
+      end
     end
 
   def update
@@ -35,7 +38,7 @@ class GroupsController < ApplicationController
     flash[:alert] = "Group deleted"
     redirect_to groups_path
   end
-  
+
 
 
   private
